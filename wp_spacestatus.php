ca22d3f6af6bd516f68728069d56bb12ed925039
Michael Wendland <accounts@michiwend.com>
2014-09-21 00:33:55 +0200

    adds wp_spacestatus.php

---
+++ wp_spacestatus.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WP SpaceStatus
+ * Plugin URI: https://github.com/michiwend/wp_spacestatus
+ * Description: a WordPress plugin that displays your space status.
+ * Version: 1.0.0
+ * Author: Michael Wendland
+ * Author URI: http://blog.michiwend.com
+ * License: GPL2
+ */
+
+
+?>
